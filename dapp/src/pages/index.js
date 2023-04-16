8f509b14825aa2183171a68195843f59cd326b05
rramires <ricardo@flexbr.com>
2023-04-16 16:45:32 -0300

    Add conditional menu

--- dapp/src/pages/index.js
+++ dapp/src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import Footer from '@/components/Footer'
 import { useState } from 'react';
 import { doLogin } from '@/services/Web3Service'
@@ -25,23 +26,42 @@ export default function Home() {
       </Head>
       <div className='container px-4 py-5'>
         <div className='row flex-lg-row-reverse align-items-center py-5 g-5'>
-          <div className='col-10 col-sm-8 col-lg-6'>
-            <img className='d-block mx-lg-auto img-fluid' width='700' height='500' src='/beggarBear.jpg' />
-          </div>
+          
+            {
+              !wallet ? (
+                <div className='col-10 col-sm-8 col-lg-6'>
+                  <img className='d-block mx-lg-auto img-fluid' width='700' height='500' src='/beggarBear.jpg' />
+                </div>
+              ) : (
+                <div className='col-10 col-sm-8 col-lg-6'>
+                  <p className='mb-3'>Welcome: {wallet}</p>
+                  <p className='mb-3'>What do you want?</p>
+                  <div className='col-12'>
+                    <p><Link className='btn btn-primary col6 p-3' href='/donate'>Make a Donation</Link></p>
+                    <p><Link className='btn btn-secondary col6 p-3' href='/create'>Create a Campaign</Link></p>
+                  </div>
+                </div>
+              )
+            }
           <div className='col-lg-6'>
             <h1 className='display-5 fw-bold text-body-emphasis lh-1 mb-3'>Donate Crypto !</h1>
             <p className='lead'>Your decentralized donation platform.</p>
             <p className='lead mb-3'>Authenticate with your wallet, create your campaign or donate to existing campaigns.</p>
-            <div className='d-grid gap-2 d-md-flex justify-content-md-start'>
-              <button className='btn btn-primary btn-lg px-4 me-md-2' 
-                      type='button'
-                      onClick={onConnectClick}>
-                <img className='me-3' width='64' src='/metamask.svg' />
-                Connect with your wallet
-              </button>
-            </div>
-            {wallet ?? `Wallet Address:${wallet}`}
-            {error ?? `Error:${error}`}
+            {
+              !wallet ? (
+                <div className='d-grid gap-2 d-md-flex justify-content-md-start'>
+                  <button className='btn btn-primary btn-lg px-4 me-md-2' 
+                          type='button'
+                          onClick={onConnectClick}>
+                    <img className='me-3' width='64' src='/metamask.svg' />
+                    Connect with your wallet
+                  </button>
+                  { error }
+                </div>
+              ) : (
+                <></>
+              )
+            }
           </div>
         </div>
         <Footer />
